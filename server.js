89948f41298d866d401f2a9ed380e1b39431b738
onomatopoetica <72478102+onomatopoetica@users.noreply.github.com>
2021-03-24 11:05:42 -0700

    Updating port info

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3002;
+const PORT = process.env.PORT || 3002
 
 const app = express();
 
